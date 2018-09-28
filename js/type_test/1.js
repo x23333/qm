8f9073ba61905df274e3289e196088f8041cc55a
x23333 <2393274999@qq.com>
2018-09-28 21:19:45 +0800

    sheji

---
+++ js/type_test/1.js
@@ -0,0 +1,32 @@
+var und = undefined;
+var nul = null;
+var boo = true;
+var num = 1;
+var str = 'str';
+//Sybmol
+
+// 复杂数据类型 引用类型(被赋值时是新建一个指针指向原数据)
+var obj = {a:1};
+var func = function (){};
+var arr = [1,2,3];
+// // obj1 = obj;
+// var obj1 = {};
+// for(ikkvar k in obj){
+//     obj1[k] = obj[k];
+// }
+// obj1.a = 2;
+// console.log(obj1);
+// console.log('--------');
+// console.log(obj);
+
+console.log(typeof und);
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof boo);
+console.log(typeof nul);
+
+
+
+console.log(typeof func);
+
+console.log(typeof arr);
