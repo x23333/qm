6eda5268e6ad738bab4166218345a7191a1f7c8e
x23333 <2393274999@qq.com>
2018-10-22 21:35:18 +0800

    车-globalData传数据

--- wxapp/app.js
+++ wxapp/app.js
@@ -1,39 +1,25 @@
-//app.js
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+  onLaunch(){
+    console.log('开始加载');
+    wx.request({
+      url:'https://www.easy-mock.com/mock/5bca925ce6742c1bf8220bee/ikcamp/video',
+      success: (response) => {
+        // const{slides, vehicles,stories} = response.data;
+        // console.log(slides, vehicles,stories);
+        // this.globalData.slides = slides
+        // this.globalData.vehicles = vehicles
+        // this.globalData.stories = stories
+        // let a = {};
+        Object.assign(this.globalData, response.data,{
+          'faker': 'god'
+        });
+        console.log(this.globalData);
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
       }
     })
   },
+  // // 全局的数据 App.js 是全局的， 如用户的登录状态
   globalData: {
-    userInfo: null
+    
   }
 })
--- wxapp/pages/stories/index.js
+++ wxapp/pages/stories/index.js
@@ -2,10 +2,28 @@ const app = getApp()
 
 Page({
   data: {
-
+    stories: [],
+    currentVid: null,
+    currentVideo: null
   },
   // 生命周期
   onLoad(options) {
-    console.log('onload');
+    this.setData({
+      stories: app.globalData.stories
+    })
+  },
+  play(event) {
+    if(this.data.currentVid !== null){
+      this.data.currentVideo.pause();
+    }
+    const vid = event.currentTarget.dataset.vid
+    // console.log(vid);
+    const currentVideo = wx.createVideoContext(`${vid}`)
+    currentVideo.play()
+
+    this.setData({
+      currentVideo,
+      currentVid: vid
+    })
   }
 });
