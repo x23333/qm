719a98c25c5d0d70ec6e129d7c04a6adbd6d7e5e
x23333 <2393274999@qq.com>
2018-11-15 18:41:00 +0800

    布局 京东商城

--- yys/miniprogram/pages/product/product.js
+++ yys/miniprogram/pages/product/product.js
@@ -1,66 +1,33 @@
-// miniprogram/pages/product/product.js
+wx.cloud.init({
+})
+const db = wx.cloud.database();
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    name: '',
+    product: {}
+  },
+  onLoad (options) {
+    let name = options.title;
+    console.log(name)
+    wx.setNavigationBarTitle({
+      title: '商品详情'
+    });
 
+    db.collection("product")
+      .where({
+        name: name
+      })
+      .get({
+        success: res => {
+          console.log(res.data[0]);
+          this.setData({
+            product: res.data[0]
+
+          })
+        }
+      })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
--- yys/miniprogram/pages/productList/productList.js
+++ yys/miniprogram/pages/productList/productList.js
@@ -17,107 +17,25 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options);
+    // console.log(options);
     let lei = options.base;
-    console.log(lei)
+    // console.log(lei)
     wx.setNavigationBarTitle({
       title: options.title
     });
     db.collection("productlist")
       .where({
-        lei: "pat"
+        lei: lei
       })
       .get({
         success: res => {
+          // console.log(res)
           this.setData({
             list: res.data
           })
         }
       })
-    // this.setData({
-    //   list: db.collection("productlist")
-    //   .where({
-    //     "lei": "pat"
-    //   })    
-    // })
-    // .get(res => {
-    //   console.log(res);
-    //   this.setData({
-    //     list: res.data
-    //   })
-    // })
-    // .get({
-    //   success: res => {
-    //     console.log(res);
-    //     res.data.where({
-    //       lei: "pat"
-    //     })
-    //     .get(res => {
-    //       console.log(res);
-    //       this.setData({
-    //         list: res.data
-    //       })
-    //     })
-    //   }
-    // })
-  },
-  getData: function(){
-    // let li = [];
-    // wx.cloud.callFunction({
-    //   name: 'getData'
-    // }).then(res => {
-    //   li = res
-    // })
-    // console.log(li)
-  },
-
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
